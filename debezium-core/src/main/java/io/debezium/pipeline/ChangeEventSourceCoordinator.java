ec70633cb741a60fb91f8f8466d0af0662f8efac
Josh Ribera <jribera@sugarcrm.com>
2021-03-17 14:27:42 -0400

    DBZ-2975-18 Move executor logic within loops

--- debezium-core/src/main/java/io/debezium/pipeline/ChangeEventSourceCoordinator.java
+++ debezium-core/src/main/java/io/debezium/pipeline/ChangeEventSourceCoordinator.java
@@ -6,6 +6,7 @@
 package io.debezium.pipeline;
 
 import java.time.Duration;
+import java.util.HashMap;
 import java.util.Map;
 import java.util.Optional;
 import java.util.concurrent.ExecutorService;
@@ -87,23 +88,11 @@ public class ChangeEventSourceCoordinator<P extends TaskPartition, O extends Off
 
     public synchronized void start(CdcSourceTaskContext taskContext, ChangeEventQueueMetrics changeEventQueueMetrics,
                                    EventMetadataProvider metadataProvider) {
-        O tempOffset = null;
-        P tempPartition = null;
-
-        // TODO: iterate over all entries instead of breaking
-        for (Map.Entry<P, O> entry : previousOffsetContext.getOffsets().entrySet()) {
-            tempPartition = entry.getKey();
-            tempOffset = entry.getValue();
-            break;
-        }
         AtomicReference<LoggingContext.PreviousContext> previousLogContext = new AtomicReference<>();
         try {
             this.snapshotMetrics = changeEventSourceMetricsFactory.getSnapshotMetrics(taskContext, changeEventQueueMetrics, metadataProvider);
             this.streamingMetrics = changeEventSourceMetricsFactory.getStreamingMetrics(taskContext, changeEventQueueMetrics, metadataProvider);
             running = true;
-
-            P partition = tempPartition;
-            O previousOffset = tempOffset;
 
             // run the snapshot source on a separate thread so start() won't block
             executor.submit(() -> {
@@ -115,26 +104,34 @@ public class ChangeEventSourceCoordinator<P extends TaskPartition, O extends Off
 
                     ChangeEventSourceContext context = new ChangeEventSourceContextImpl();
                     LOGGER.info("Context created");
+                    P partition;
+                    O previousOffset;
+
+                    HashMap<P, O> partitionState = new HashMap<>();
 
                     SnapshotChangeEventSource<P, O> snapshotSource = changeEventSourceFactory.getSnapshotChangeEventSource(snapshotMetrics);
-                    CatchUpStreamingResult catchUpStreamingResult = executeCatchUpStreaming(context, previousOffset, snapshotSource);
-                    if (catchUpStreamingResult.performedCatchUpStreaming) {
-                        streamingConnected(false);
-                        commitOffsetLock.lock();
-                        streamingSource = null;
-                        commitOffsetLock.unlock();
-                    }
-                    eventDispatcher.setEventListener(snapshotMetrics);
-                    SnapshotResult<O> snapshotResult = snapshotSource.execute(context, partition, previousOffset);
-                    LOGGER.info("Snapshot ended with {}", snapshotResult);
+                    for (Map.Entry<P, O> entry : previousOffsetContext.getOffsets().entrySet()) {
+                        partition = entry.getKey();
+                        previousOffset = entry.getValue();
 
-                    if (snapshotResult.getStatus() == SnapshotResultStatus.COMPLETED || schema.tableInformationComplete()) {
-                        schema.assureNonEmptySchema();
+                        eventDispatcher.setEventListener(snapshotMetrics);
+                        SnapshotResult<O> snapshotResult = snapshotSource.execute(context, partition, previousOffset);
+                        LOGGER.info("Snapshot ended with {}", snapshotResult);
+
+                        if (snapshotResult.getStatus() == SnapshotResultStatus.COMPLETED || schema.tableInformationComplete()) {
+                            schema.assureNonEmptySchema();
+                        }
+                        partitionState.put(partition, snapshotResult.getOffset());
                     }
 
-                    if (running && snapshotResult.isCompletedOrSkipped()) {
-                        previousLogContext.set(taskContext.configureLoggingContext("streaming"));
-                        streamEvents(context, partition, snapshotResult.getOffset());
+                    for (Map.Entry<P, O> entry : partitionState.entrySet()) {
+                        partition = entry.getKey();
+                        previousOffset = entry.getValue();
+
+                        if (running) {
+                            previousLogContext.set(taskContext.configureLoggingContext("streaming"));
+                            streamEvents(context, partition, previousOffset);
+                        }
                     }
                 }
                 catch (InterruptedException e) {
