69723734ab295aa35d5030d04ac3b51ce0b45ac3
Jacob Gminder <jacob.gminder@sugarcrm.com>
2021-11-18 16:41:25 -0500

    CXP-1566: Change "No maximum LSN recorded" log message to debug level

--- debezium-connector-sqlserver/src/main/java/io/debezium/connector/sqlserver/SqlServerStreamingChangeEventSource.java
+++ debezium-connector-sqlserver/src/main/java/io/debezium/connector/sqlserver/SqlServerStreamingChangeEventSource.java
@@ -177,11 +177,11 @@ public class SqlServerStreamingChangeEventSource implements StreamingChangeEvent
                             }
                         }
                         catch (SQLException e) {
-                            LOGGER.warn("No maximum LSN recorded in the database; this may happen if there are no changes recorded in the change table yet or " +
+                            LOGGER.debug("No maximum LSN recorded in the database; this may happen if there are no changes recorded in the change table yet or " +
                                     "low activity database where the cdc clean up job periodically clears entries from the cdc tables. " +
                                     "Otherwise, this may be an indication that the SQL Server Agent is not running. " +
                                     "You should follow the documentation on how to configure SQL Server Agent running status query.");
-                            LOGGER.warn("Cannot query the status of the SQL Server Agent", e);
+                            LOGGER.debug("Cannot query the status of the SQL Server Agent", e);
                         }
                         checkAgent = false;
                     }
